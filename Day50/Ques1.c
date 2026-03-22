c1aa85c70c38ac038b2394bb1a4819ccaf88ac7f
Sanchita <sanchitaluthra10@gmail.com>
2026-03-22 22:51:49 +0530

    Add files via upload

---
+++ Day50/Ques1.c
@@ -0,0 +1,55 @@
+/*
+Problem: BST Search
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct Node* search(struct Node* root, int key) {
+    if (root == NULL || root->data == key)
+        return root;
+
+    if (key < root->data)
+        return search(root->left, key);
+
+    return search(root->right, key);
+}
+
+int main() {
+    struct Node* root = newNode(4);
+    root->left = newNode(2);
+    root->right = newNode(7);
+    root->left->left = newNode(1);
+    root->left->right = newNode(3);
+
+    int key = 3;
+
+    struct Node* result = search(root, key);
+
+    if (result != NULL)
+        printf("Found\n");
+    else
+        printf("Not Found\n");
+
+    return 0;
+}
